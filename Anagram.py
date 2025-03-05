a9c8742ba1782867f0e8a93a457fb5e02b13372f
Krzysztof Majewski <majoss97@gmail.com>
2025-03-05 13:09:00 +0100

    Update Anagram.py

--- Anagram.py
+++ Anagram.py
@@ -4,15 +4,18 @@ def main():
     print("Enter 2 words and I'll tell whether they're anagrams")
 
     while True:
-        first_word = input("Gimme first word")
-        second_word = input("Gimme second word")
+        first_word = input("Gimme first word\n")
+        second_word = input("Gimme second word\n")
         first_array = sorted(list(first_word))
         second_array = sorted(list(second_word))
         if first_array == second_array:
             print("They're obviously anagrams")
         else:
             print("They're not anagrams")
-
+        decision = input("Do you want to continue? (Y/N)\n")
+        if decision.lower() == "n":
+            print("Bye then")
+            break
 
 
 if __name__ == "__main__":
